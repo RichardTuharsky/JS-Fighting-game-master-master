bd0fd21bef4505f0eb46a460f8e8ca71926dfb20
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2022-06-12 17:39:30 +0200

    .

--- JS-Fighting-game-master-master/JS-Fighting-game-master/index.js
+++ JS-Fighting-game-master-master/JS-Fighting-game-master/index.js
@@ -21,12 +21,23 @@ class Sprite {
         this.scale = scale
         this.framesMax = framesMax
         this.framesCurrent = 0
+        this.framesElapsed = 0
+        this.framesHold = 8
     }
     draw() {
       c.drawImage(this.image, this.framesCurrent * (this.image.width / this.framesMax), 0, this.image.width / this.framesMax, this.image.height, this.position.x, this.position.y, (this.image.width / this.framesMax) * this.scale, this.image.height * this.scale) 
     }
     update() {
         this.draw()
+        this.framesElapsed++
+
+        if(this.framesElapsed % this.framesHold === 0){
+          if(this.framesCurrent < this.framesMax - 1) {
+            this.framesCurrent++
+          } else {
+            this.framesCurrent = 0
+          }
+        }
     }
   }
 
